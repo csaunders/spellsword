7a127aa80ba20455cd482ea584942803f430dbeb
Chris Saunders <chris.saunders@shopify.com>
2014-01-26 00:24:27 +0000

    gets tests passing. this took took long and i am a sad

--- collider.lua
+++ collider.lua
@@ -24,34 +24,13 @@ end
 function Collider:performBoundsCheck(character)
   local within = true
   for i, bound in ipairs(self.objects) do
-    local points = bound['points']
-    local size = table.getn(points)
-    for i=1,size do
-      local A, B, C, D = nil, nil, nil, nil
-      local formatString = "(%d, %d) -- Comparing p1(%d, %d) to p2(%d, %d)"
-      local p1 = points[i % (size + 1)]
-      local p2 = points[(i + 1) % (size)]
-      p2 = p2 or points[1]
-      print(string.format(formatString, character.x, character.y, p1.x, p1.y, p2.x, p2.y))
-      A = -(p2.y - p1.y)
-      B = p2.x - p1.x
-      C = -(A*p1.x + B*p1.y)
-      D = A * character.x + B * character.y + C
+    local normalizedX = character.x - bound.x
+    local normalizedY = character.y - bound.y
 
-      -- if character.y == 15 then
-        print(string.format("A %d, B %d, C %d, D %d", A, B, C, D))
-      --   print(os)
-      --   local formatString = "%s (%d, %d)"
-      --   print(string.format(formatString, 'Character', character.x, character.y))
-      --   print(string.format(formatString, 'p1', p1.x, p1.y))
-      --   print(string.format(formatString, 'p2', p2.x, p2.y))
-      -- end
-
-      if D < 0 then
-        self:cacheBoundCheck(character)
-        within = false
-        break
-      end
+    if normalizedX > bound.width or normalizedY > bound.height then
+      self:cacheBoundCheck(character)
+      within = false
+      break
     end
   end
   return within
--- test/test_collider.lua
+++ test/test_collider.lua
@@ -2,13 +2,7 @@ require "../collider"
 
 local function setup(x, y)
   local testColliderObjects = {}
-  local singleRect = {}
-  singleRect['points'] = {
-    {['x'] = 10, ['y'] = 10},
-    {['x'] = 10, ['y'] = 15},
-    {['x'] = 15, ['y'] = 15},
-    {['x'] = 10, ['y'] = 15}
-  }
+  local singleRect = {['x'] = 10, ['y'] = 10, ['width'] = 5, ['height'] = 5}
   table.insert(testColliderObjects, singleRect)
   local testColliderCharacter = {
     ['x'] = x, ['y'] = y
@@ -29,4 +23,10 @@ end
 function test_collision_on_line_of_box()
   collider, character = setup(13, 15)
   assert_equal(true, collider:withinBounds(character))
+end
+
+function test_point_caching()
+  collider, character = setup(17, 13)
+  assert_equal(false, collider:withinBounds(character))
+  assert_equal(true, collider:knownPointOutOfBounds(character))
 end
