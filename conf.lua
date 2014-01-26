04a27915c039c5ea70e8411dac754b81165c1f4c
Chris Saunders <chris.saunders@shopify.com>
2014-01-26 20:55:34 +0000

    fixes love 0.9.0 issue

--- conf.lua
+++ conf.lua
@@ -1,9 +1,13 @@
 
 function love.conf(t)
-    t.title = "SpellSword"    -- The title of the window the game is in (string)
-    t.author = "csaunders"    -- The author of the game (string)
-    t.screen.width = 1024     -- The window width (number)
-    t.screen.height = 768     -- The window height (number)
-    t.screen.fullscreen = false   -- Enable fullscreen (boolean)
-    t.screen.vsync = false      -- Enable vertical sync (boolean)
+  t.window = t.window or t.screen
+
+  t.title = "SpellSword"    -- The title of the window the game is in (string)
+  t.author = "csaunders"    -- The author of the game (string)
+  t.screen.width = 1024     -- The window width (number)
+  t.screen.height = 768     -- The window height (number)
+  t.screen.fullscreen = false   -- Enable fullscreen (boolean)
+  t.screen.vsync = false      -- Enable vertical sync (boolean)
+
+  t.screen = t.screen or t.window
 end
