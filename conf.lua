2927593cea1dc7e4a7f95ddbdb733678dfdbbe1d
Chris Saunders <chris.saunders@shopify.com>
2014-01-26 21:24:52 +0000

    assssssss

--- conf.lua
+++ conf.lua
@@ -4,8 +4,8 @@ function love.conf(t)
 
   t.title = "SpellSword"    -- The title of the window the game is in (string)
   t.author = "csaunders"    -- The author of the game (string)
-  t.screen.width = 1024     -- The window width (number)
-  t.screen.height = 768     -- The window height (number)
+  t.window.width = 1024     -- The window width (number)
+  t.window.height = 768     -- The window height (number)
   t.screen.fullscreen = false   -- Enable fullscreen (boolean)
   t.screen.vsync = false      -- Enable vertical sync (boolean)
 
